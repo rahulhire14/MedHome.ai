52c2b1669e5b543b25aeb3277f8b06de8b4bc5c6
Rahul Hire <hirer398@gmail.com>
2026-01-20 22:36:54 +0530

    done

--- MedHome.ai/backend/profiles/models.py
+++ MedHome.ai/backend/profiles/models.py
@@ -2,9 +2,7 @@ from django.conf import settings
 from django.db import models
 
 class UserProfile(models):
-    user=models.OneToOneField(settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name="profile")
+    user=models.OneToOneField(settings.AUTH_USER_MODEL,  on_delete=models.CASCADE)
     
 
     full_name = models.CharField(max_length=100)
@@ -21,8 +19,8 @@ class UserProfile(models):
     PhoneNumber = models.CharField(max_length=15)
 
     created_on=models.DateField(auto_now_add=True)
-    updated_on =models.DateField(auto_now=True)
+    updated_on =models.DateField(auto_now_add=True)
 
 
     def __str__(self):
-     return f"{self.full_name} ({self.user.email})"
+     return self
